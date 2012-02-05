d39e40280c419c96d6a442e7e7959fd4c05c280c
Robert Middleswarth <robert@middleswarth.net>
2012-02-05 13:52:28 -0500

    Add in billable and non-billable snippet tokens

--- api/App.php
+++ api/App.php
@@ -25,3 +25,61 @@ class Cerb5BlogSnippetTokenTimeTracker implements IContextToken {
 		return $total_time_all;
 	}
 };
+
+class Cerb5BlogSnippetTokenTimeTrackerBillable implements IContextToken {
+	static function getValue($context, $context_values) {
+		$total_time_billable = -1;
+		if($context_values['id']) {
+			// Adds total time worked per ticket to the token list.
+			$db = DevblocksPlatform::getDatabaseService();
+		
+			$sql = "SELECT sum(tte.time_actual_mins) mins ";
+			$sql .= "FROM timetracking_entry tte ";
+			$sql .= "INNER JOIN context_link ON (context_link.to_context = 'cerberusweb.contexts.timetracking' ";
+			$sql .= "INNER JOIN timetracking_activity tta ON (tta.id = tte.activity_id ";
+			$sql .= "AND context_link.to_context_id = tte.id AND context_link.from_context = 'cerberusweb.contexts.ticket') ";
+			$sql .= sprintf("WHERE context_link.from_context_id =  %d ", $context_values['id']);
+			$sql .= "AND tta.rate != 0 ";
+			$sql .= "GROUP BY context_link.from_context_id ";
+            
+			$rs = $db->Execute($sql) or die(__CLASS__ . '('.__LINE__.')'. ':' . $db->ErrorMsg()); 
+			
+			if($row = mysql_fetch_assoc($rs)) {
+				$total_time_billable = intval($row['mins']);			
+			} else {
+				$total_time_billable = 0;
+			}
+	    	mysql_free_result($rs);
+		}
+		return $total_time_billable;
+	}
+};
+
+class Cerb5BlogSnippetTokenTimeTrackerNonBillable implements IContextToken {
+	static function getValue($context, $context_values) {
+		$total_time_non_billable = -1;
+		if($context_values['id']) {
+			// Adds total time worked per ticket to the token list.
+			$db = DevblocksPlatform::getDatabaseService();
+		
+			$sql = "SELECT sum(tte.time_actual_mins) mins ";
+			$sql .= "FROM timetracking_entry tte ";
+			$sql .= "INNER JOIN context_link ON (context_link.to_context = 'cerberusweb.contexts.timetracking' ";
+			$sql .= "INNER JOIN timetracking_activity tta ON (tta.id = tte.activity_id ";
+			$sql .= "AND context_link.to_context_id = tte.id AND context_link.from_context = 'cerberusweb.contexts.ticket') ";
+			$sql .= sprintf("WHERE context_link.from_context_id =  %d ", $context_values['id']);
+			$sql .= "AND tta.rate = 0 ";
+			$sql .= "GROUP BY context_link.from_context_id ";
+            
+			$rs = $db->Execute($sql) or die(__CLASS__ . '('.__LINE__.')'. ':' . $db->ErrorMsg()); 
+			
+			if($row = mysql_fetch_assoc($rs)) {
+				$total_time_non_billable = intval($row['mins']);			
+			} else {
+				$total_time_non_billable = 0;
+			}
+	    	mysql_free_result($rs);
+		}
+		return $total_time_non_billable;
+	}
+};
